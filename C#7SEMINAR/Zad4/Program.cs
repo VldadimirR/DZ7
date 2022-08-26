172977271e3d18b4e2382ccba28ec8fbd4915116
VladimirR <Vova.ru0007@yandex.ru>
2022-08-26 13:07:44 +0500

    zadanie4

---
+++ C#7SEMINAR/Zad4/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Задайте двумерный массив. Найдите сумму
+элементов, находящихся на главной диагонали (с индексами
+(0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12 */
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Sum(array);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+void Sum(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j )
+            {
+                sum = sum + array[i, j];
+            }
+        }
+    }
+    Console.WriteLine($"Сумма элементов по главной диагонали равна {sum}.");
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
